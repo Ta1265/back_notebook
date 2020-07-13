898f45b8b38412ab2b95c1de4cc96678812a9ca4
Ta1265 <tayloranderson1265@gmail.com>
2020-07-13 17:29:42 -0400

    using eb default RDS instead

--- stickytodo/settings.py
+++ stickytodo/settings.py
@@ -79,16 +79,28 @@ WSGI_APPLICATION = 'stickytodo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'notebook_2',
-        'USER': 'taylor',
-        'PASSWORD': 'cheeze1265',
-        'HOST' : 'database-1.ccamiddz2iib.us-east-1.rds.amazonaws.com',
-        'PORT' : 5432,
+if 'RDS_HOSTNAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'notebook_2',
+            'USER': 'taylor',
+            'PASSWORD': 'cheeze1265',
+            'HOST' : 'database-1.ccamiddz2iib.us-east-1.rds.amazonaws.com',
+            'PORT' : 5432,
+        }
+    }
 
 
 # Password validation
@@ -131,9 +143,3 @@ STATIC_URL = '/static/'
 STATIC_ROOT = 'static'
 
 CORS_ORIGIN_ALLOW_ALL = True
-
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000',
-    'https://notebookreact.s3.us-east-2.amazonaws.com',
-    'http://notebookreact.s3-website.us-east-2.amazonaws.com',
-]
